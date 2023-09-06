faff13243d25fce372a175c4f61cb552c58a9c25
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-09-06 00:24:29 -0400

    Create basic 'Ship' Class and tests

--- src/ship.js
+++ src/ship.js
@@ -1,2 +1,42 @@
 // Ship Module
 
+/** Ship Constructor */
+export class Ship {
+	/**
+	 * The ship has properties
+	 * - `len`gth
+	 * - hits
+	 * - sunk {boolean}
+	 *
+	 * @param {string} name Name of ship:
+	 * - Carrier
+	 * - Battle
+	 * - Destroyer
+	 * - Sub
+	 * - Patrol
+	 * @param {number} len Length
+	 * - Represents length of ship in `units` on the gameboard
+	 */
+	constructor(name, len) {
+		this.name = name;
+		this.length = len;
+		this.hits = 0;
+		this.sunk = false;
+	}
+
+	/**
+	 * Increment hits property
+	 * @return {number} Number of hits
+	 */
+	hit = () => ++this.hits;
+
+	/**
+	 * Check if ship is sunk
+	 * - Is this ship sunk?
+	 *
+	 * @return {boolean}
+	 */
+	isSunk = () => {
+		return this.hits >= this.length ? true : false;
+	};
+}
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,2 +1,19 @@
 // Ship Test Module
 
+import {Ship} from '../ship';
+
+describe('Ship Constructor', () => {
+	const testShip = new Ship(5);
+
+	test('Hit sucessful', () => {
+		expect(testShip.hit()).toBe(1);
+	});
+
+	test('Ship Sinks', () => {
+		const smallShip = new Ship(2);
+		smallShip.hit();
+		smallShip.hit();
+
+		expect(smallShip.isSunk()).toBe(true);
+	});
+});
