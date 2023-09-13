6c47420c0f33c5afc2fc88bbb4bb9f3611fceea2
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-09-13 00:16:28 -0400

    Add 'renderGameboards' && 'addMiss' funcs to render the current players gameboard to DOM

--- src/dom.js
+++ src/dom.js
@@ -6,8 +6,38 @@ import battle from './assets/img/battle-ship.svg'; // 4
 import destroy from './assets/img/destroyer.svg'; // 3
 import sub from './assets/img/submarine.svg'; // 3
 import patrol from './assets/img/patrol.svg'; // 2
+import xIcon from './assets/img/x-icon.svg';
+import {Player, Computer} from './player';
+import {player1, player2} from '.';
 
 export const Dom = (() => {
+	/**
+	 * Add a marker (X) to a cell where a miss occurred
+	 *
+	 * @param {number[]} coord Coordinates
+	 * - Where? Which cell/space
+	 * @param {boolean} boardIsOpponent Is the board
+	 * you are adding a marker to the opponents? (true)
+	 */
+	const addMiss = (coord, boardIsOpponent) => {
+		let cell;
+		if (boardIsOpponent) {
+			cell = document.querySelector(`#player2 .col[data="${coord[0]}"]`)
+				.querySelector(`.cell[data="${coord[1]}"]`);
+		} else {
+			cell = document.querySelector(`#player1 .col[data="${coord[0]}"]`)
+				.querySelector(`.cell[data="${coord[1]}"]`);
+		}
+
+		// Create X image to place in cell
+		const X = new Image(getCellWidth() - 10);
+		X.src = xIcon;
+		X.alt = 'Cell has been shot';
+		X.classList.add('x-icon');
+
+		cell.appendChild(X);
+	};
+
 	/**
 	 * Add the ships to the DOM
 	 * (NOT on the gameboard)
@@ -24,30 +54,35 @@ export const Dom = (() => {
 		airCarrier.classList.add('aircraft-carrier');
 		airCarrier.src = carrier;
 		airCarrier.alt = 'Aircraft Carrier';
+		airCarrier.setAttribute('data', '5');
 
 		// Battle Ship
 		const battleShip = new Image(getCellWidth());
 		battleShip.classList.add('battle-ship');
 		battleShip.src = battle;
 		battleShip.alt = 'Battle Ship';
+		battleShip.setAttribute('data', '4');
 
 		// Destroyer
 		const destroyer = new Image(getCellWidth());
 		destroyer.classList.add('destroyer');
 		destroyer.src = destroy;
 		destroyer.alt = 'Destroyer';
+		destroyer.setAttribute('data', '3');
 
 		// Submarine
 		const submarine = new Image(getCellWidth());
 		submarine.classList.add('submarine');
 		submarine.src = sub;
 		submarine.alt = 'Submarine';
+		submarine.setAttribute('data', '3');
 
 		// Patrol Boat
 		const patrolBoat = new Image(getCellWidth());
 		patrolBoat.classList.add('patrol-boat');
 		patrolBoat.src = patrol;
 		patrolBoat.alt = 'Patrol Boat';
+		patrolBoat.setAttribute('data', '2');
 
 		// Add to container
 		shipContainer.appendChild(airCarrier);
@@ -59,6 +94,55 @@ export const Dom = (() => {
 
 		// Allow ships to be 'dragged'
 		dragDrop.makeShipsDraggable();
+	};
+
+	/**
+	 * Render `player`'s board based on their
+	 * `Gameboard` Object
+	 * @link See 'Player.board'
+	 *
+	 * @param {Player} player Current Player
+	 * @param  {Player | Computer} opponent Your opponent
+	 * - You dont want to render the current opponents ships
+	 */
+	const renderGameboards = (player, opponent) => {
+		const playerBoard = player.board.grid;
+
+		// Loop through Players Gameboard backwards to place ships correctly
+		for (let x = 9; x >= 0; x--) {
+			for (let y = 0; y < 10; y++) {
+				const cell = playerBoard[x][y];
+				// Get the Actual cell on DOM from `cell`'s coordinates
+				const realCell =
+					document.querySelector(`#player1 .col[data="${cell.coord[0]}"]`)
+						.querySelector(`.cell[data="${cell.coord[1]}"]`);
+
+				// If Cell has a ship on it
+				if (cell.hasShip) {
+					// Place ship inside cell
+					const shipName = cell.ship.name;
+					const shipImg = document.querySelector(`.${shipName}`);
+
+					realCell.appendChild(shipImg);
+				// If cell doesnt have ship but HAS been shot
+				} else if (cell.shot) {
+					addMiss(cell.coord, false);
+				}
+			}
+		}
+
+		// Get opponent Board
+		const oppBoard = opponent.board.grid;
+
+		// Loop through opponents board to place markers on shot spaces
+		for (const col of oppBoard) {
+			for (const cell of col) {
+				// If cell has been shot
+				if (cell.shot) {
+					addMiss(cell.coord, true);
+				}
+			}
+		}
 	};
 
 	/**
@@ -103,11 +187,16 @@ export const Dom = (() => {
 		 * and append the element to the Events target
 		 * (where you dropped it)
 		 * @param {Event} event Drop Event
+		 *
+		 * @return {HTMLImageElement} The Ship Image being dropped
 		 */
 		const onDrop = (event) => {
 			event.preventDefault();
-			const data = event.dataTransfer.getData('text');
-			event.target.appendChild(document.querySelector(`[src="${data}"]`));
+			const data = event.dataTransfer.getData('text'); // Get data
+			const ship = document.querySelector(`[src="${data}"]`); // Get ship
+			event.target.appendChild(ship); // Add ship to cell
+
+			return ship;
 		};
 
 		return {makeShipsDraggable, onDrop};
@@ -116,5 +205,5 @@ export const Dom = (() => {
 	/** @return {number} Gameboard Cell's width */
 	const getCellWidth = () => document.querySelector('.cell').clientWidth;
 
-	return {createShips, dragDrop};
+	return {createShips, dragDrop, renderGameboards, addMiss};
 })();
